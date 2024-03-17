5b2ee4cfe7867130c8037e0d5e808d3cb8f7b7fc
agolubinskiyS <agolubinskiy@stratio.com>
2024-03-17 12:31:08 +0100

    Add authentication

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,5 +5,9 @@ for IP in "${IPS[@]}"; do
     echo "Allow $IP" >> /etc/tinyproxy/tinyproxy.conf
 done
 
+if [ "$BASIC_AUTH" = "true" ]; then
+  echo "BasicAuth $USERNAME $PASSWORD" >> /etc/tinyproxy/tinyproxy.conf
+fi
+
 echo "Running tinyproxy server..."
 /usr/bin/tinyproxy -d
